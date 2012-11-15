daf30d183835d394160190f35e38e5c625c4056f
Justin Obney <Justin Obney>
2012-11-15 13:41:53 -0600

    Initial Add after working on Kata | Day 1 of String Calculator

---
+++ Daily_TDD_Kata/StringCalculator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Daily_TDD_Kata
+{
+    public class StringCalculator
+    {
+        public static int Add(string numbers)
+        {
+            int result = 0;
+
+            // An empty string always yields 0.
+            if (numbers == "") return 0;
+
+            // Standard Delimiters
+            List<string> delimiters = new List<string>(){ ",", @"\n" };
+
+            // Finds delimiters that batch the //[delimiter] pattern and adds them
+            Match dynamic_delimiters = new Regex(@"^//[\D]?").Match(numbers);
+            if (dynamic_delimiters.Success)
+            {
+                numbers = numbers.Replace(dynamic_delimiters.Groups[0].Value, "");
+                delimiters.Add(dynamic_delimiters.Groups[0].Value.Replace(@"//", ""));
+            }
+
+            // Checks for the existance of any delimiters else assumes it is a parsable number
+            if (delimiters.Any(numbers.Contains))
+            {
+                IEnumerable<string> number_arr = (IEnumerable<string>)numbers.Split(delimiters.ToArray(), StringSplitOptions.None);
+                result = number_arr.Select(s => int.Parse(s)).Sum();
+            }
+            else
+            {
+                result = int.Parse(numbers);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Daily_TDD_Kata_Tests/StringCalculatorTests.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Text;
+using Daily_TDD_Kata;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Daily_TDD_Kata_Tests
+{
+    [TestClass]
+    public class StringCalculatorTests
+    {
+        [TestClass]
+        public class TheAddMethod
+        {
+            [TestMethod]
+            public void ReturnZeroForEmptyString()
+            {
+                // arrange
+                string empty_string = "";
+                int expected = 0;
+
+                // act
+                int actual = StringCalculator.Add(empty_string);
+
+                // assert
+                Assert.AreEqual(expected, actual);
+                
+            }
+
+            [TestMethod]
+            public void ReturnOneForStringOne()
+            {
+                // arrange
+                string numbers = "1";
+                int expected = 1;
+
+                // act
+                int actual = StringCalculator.Add(numbers);
+
+                // assert
+                Assert.AreEqual(expected, actual);
+                
+            }
+
+            [TestMethod]
+            public void ReturnThreeForStringOneTwo()
+            {
+                // arrange
+                string numbers = "1,2";
+                int expected = 3;
+
+                // act
+                int actual = StringCalculator.Add(numbers);
+
+                // assert
+                Assert.AreEqual(expected, actual);
+
+            }
+
+            [TestMethod]
+            public void ReturnSixForStringOneTwoTree()
+            {
+                // arrange
+                string numbers = "1,2,3";
+                int expected = 6;
+
+                // act
+                int actual = StringCalculator.Add(numbers);
+
+                // assert
+                Assert.AreEqual(expected, actual);
+
+            }
+
+            [TestMethod]
+            public void ReturnSixForStringWithMultipleDelimeters()
+            {
+                // arrange
+                string numbers = @"1\n2,3";
+                int expected = 6;
+
+                // act
+                int actual = StringCalculator.Add(numbers);
+
+                // assert
+                Assert.AreEqual(expected, actual);
+
+            }
+
+            [TestMethod]
+            public void ReturnSixForStringSupportingDifferentDelimeters()
+            {
+                // arrange
+                string numbers = @"//;1\n2,3";
+                int expected = 6;
+
+                // act
+                int actual = StringCalculator.Add(numbers);
+
+                // assert
+                Assert.AreEqual(expected, actual);
+
+            }
+        }
+    }
+}
