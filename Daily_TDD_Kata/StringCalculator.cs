65fff5e8cc222b4285012529c18c65fec24d853a
Justin Obney <justin@envoc.com>
2012-11-30 08:37:34 -0600

    Working on TDD.
    Focusing on:
    -- LINQ Grouping Methods
    -- Resharper usage

--- Daily_TDD_Kata/StringCalculator.cs
+++ Daily_TDD_Kata/StringCalculator.cs
@@ -7,40 +7,27 @@ namespace Daily_TDD_Kata
 {
     public class StringCalculator
     {
-        public static int Add(string numbers)
+        public int Add(string numbers)
         {
-            // If the input is an empty string, always yeild 0
-            if(numbers == "")
-                return 0;
+            var result = 0;
 
-            // Define standard delimiters
-            List<string> accepted_delimiters = new List<string>() { ",", @"\n" };
+            //Return 0 when an empty string is sent in
+            if (string.IsNullOrEmpty(numbers))
+                return result;
 
-            // Search the input string for delimiters noted 
-            // Like this:   "//[delimiter]\n[numbers…]" for example "//;\n1;2" should return three where the default delimiter is ';' .
-            Match dynamic_delimiter_worker = new Regex(@"^//[\D]\\n").Match(numbers);
-
-            if(dynamic_delimiter_worker.Success){
-                // Add matched group value, removing the // and \n
-                string cleaned_delimiter = Regex.Replace(dynamic_delimiter_worker.Groups[0].Value, @"//|\\n", "", RegexOptions.IgnoreCase);
-                accepted_delimiters.Add(cleaned_delimiter);
-
-                // Remove the // from the original input
-                numbers = numbers.Replace(dynamic_delimiter_worker.Groups[0].Value, "");
-            }
-
-            // Convert the number to an IEnumerable to more easily work against the collection
-            IEnumerable<int> number_worker = numbers
-                .Split(accepted_delimiters.ToArray(), StringSplitOptions.RemoveEmptyEntries)
-                .Where(number => int.Parse(number) <= 1000)  // Ignore numbers greater than 1000
-                .Select(number => int.Parse(number));
-
-            if (number_worker.Any(n => n < 0))
+            string[] standardDelimiters = {",", @"\n"};
+            
+            if (standardDelimiters.Any(numbers.Contains))
             {
-                throw new Exception("Negative numbers are not allowed");
+                IEnumerable<string> splitNums = numbers.Split(standardDelimiters, StringSplitOptions.None);
+                result = splitNums.Select(int.Parse).Sum();
+            }
+            else
+            {
+                result = int.Parse(numbers);
             }
 
-            return number_worker.Sum();
+            return result;
         }
     }
 }
--- Daily_TDD_Kata_Tests/StringCalculatorTests.cs
+++ Daily_TDD_Kata_Tests/StringCalculatorTests.cs
@@ -8,140 +8,90 @@ namespace Daily_TDD_Kata_Tests
     [TestClass]
     public class StringCalculatorTests
     {
-        [TestClass]
-        public class TheAddMethod
+        protected StringCalculator Target;
+
+        [TestInitialize]
+        public void Init()
         {
-            // The method can take 0, 1 or 2 numbers, and will return their sum (for an empty string it will return 0) for example "" or "1" or "1,2"
+            Target = new StringCalculator();
+        }
+
+        [TestClass]
+        public class TheAddMethod : StringCalculatorTests
+        {
+            // The method can take 0, 1 or 2 numbers, and will return their sum 
+            // (for an empty string it will return 0) for example "" or "1" or "1,2"
+
             [TestMethod]
-            public void return_zero_for_empty_string()
+            public void ReturnZeroForEmptyString()
             {
                 // arrange
                 string input_string = "";
                 int expected = 0;
 
                 // act
-                int actual = StringCalculator.Add(input_string);
+                int actual = Target.Add(input_string);
 
                 // assert
                 Assert.AreEqual(expected, actual);
             }
 
             [TestMethod]
-            public void return_one_for_string_one()
+            public void ReturnOneForStringOne()
             {
                 // arrange
-                int expected = 1;
                 string input_string = "1";
+                int expected = 1;
 
                 // act
-                int actual = StringCalculator.Add(input_string);
+                int actual = Target.Add(input_string);
 
                 // assert
                 Assert.AreEqual(expected, actual);
-                
             }
 
             [TestMethod]
-            public void return_three_for_comma_seperated_numbers_one_two()
+            public void ReturnsThreeForStringOneTwo()
             {
                 // arrange
-                int expected = 3;
                 string input_string = "1,2";
-
-                // act
-                int actual = StringCalculator.Add(input_string);
-
-                // assert
-                Assert.AreEqual(expected, actual);
-                
-            }
-
-            // Allow the Add method to handle an unknown amount of numbers
-            [TestMethod]
-            public void return_ten_for_comma_seperated_numbers_one_two_three_four()
-            {
-                // arrange
-                int expected = 10;
-                string input_string = "1,2,3,4";
-
-                // act
-                int actual = StringCalculator.Add(input_string);
-
-                // assert
-                Assert.AreEqual(expected, actual);
-                
-            }
-
-            // Allow the Add method to handle new lines between numbers (instead of commas).
-            // The following input is ok:  "1\n2,3"  (will equal 6)
-            [TestMethod]
-            public void allow_add_to_handle_new_line_as_delimiter()
-            {
-                // arrange
-                int expected = 6;
-                string input_string = @"1\n2,3";
-
-                // act
-                int actual = StringCalculator.Add(input_string);
-
-                // assert
-                Assert.AreEqual(expected, actual);
-            }
-
-            // Support different delimiters
-
-            // To change a delimiter, the beginning of the string will contain a separate line that looks 
-            // like this:   "//[delimiter]\n[numbers…]" for example "//;\n1;2" should return three where the default delimiter is ';' .
-
-            [TestMethod]
-            public void allow_delimiter_to_be_defined_in_input_string_using_slash_delimiter_new_line_format()
-            {
-                // arrange
                 int expected = 3;
-                string input_string = @"//;\n1;2";
 
                 // act
-                int actual = StringCalculator.Add(input_string);
+                int actual = Target.Add(input_string);
 
                 // assert
                 Assert.AreEqual(expected, actual);
-                
             }
-
-            // Calling Add with a negative number will throw an exception “negatives not allowed” - and the negative that was passed.
-            // If there are multiple negatives, show all of them in the exception message
 
             [TestMethod]
-            [ExpectedException(typeof (Exception), "Negative numbers are not allowed")]
-            public void throws_exception_for_negative_numbers()
+            public void ReturnsSixForStringOneTwoThree()
             {
                 // arrange
-                int expected = 2;
-                string input_string = "4,-2";
+                string input_string = "1,2,3";
+                int expected = 6;
 
                 // act
-                int actual = StringCalculator.Add(input_string);
+                int actual = Target.Add(input_string);
 
                 // assert
                 Assert.AreEqual(expected, actual);
-                
             }
 
-            // Numbers bigger than 1000 should be ignored, so adding 2 + 1001  = 2
             [TestMethod]
-            public void ignore_numbers_greater_than_one_thousand()
+            public void ReturnsSixAloowingNewLineDelimiter()
             {
                 // arrange
-                int expected = 2;
-                string input_string = "2,1001";
+                string input_string = @"1\n2,3";
+                int expected = 6;
 
                 // act
-                int actual = StringCalculator.Add(input_string);
+                int actual = Target.Add(input_string);
 
                 // assert
                 Assert.AreEqual(expected, actual);
-                
             }
+            
         }
     }
 }
